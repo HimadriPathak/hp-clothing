8791b18fda942e186e9773888dbf912a805a257c
Himadri Pathak <pathak.himadri7458@gmail.com>
2025-05-03 10:42:47 +0530

    stripe integration

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,10 +1,13 @@
-import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-
 import { useSelector } from "react-redux";
+
 import {
   selectCartItems,
   selectCartTotal,
 } from "../../store/cart/cart.selector";
+
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
+
 import {
   CheckoutContainer,
   CheckoutHeader,
@@ -39,6 +42,8 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
